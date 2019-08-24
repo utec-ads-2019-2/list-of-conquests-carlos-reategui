da26fa655cbf94a4625e14fe001907e87bc8cfac
carlos-reategui <carlos.reategui@utec.edu.pe>
2019-08-24 11:21:51 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <stdio.h>
 
-using namespace std;
+std::string firstWord(const std::string& line) {
+    return line.substr(0, line.find(' '));
+}
 
-int main(int argc, char *argv[]) {
+int main() {
+    unsigned int n; std::cin >> n;
+    std::map<std::string, int> countries;
+    std::string txt;
+    std::cin.ignore();
+
+    for(unsigned int i = 0; i < n; i++) {
+        getline(std::cin, txt);
+        countries[firstWord(txt)]++;
+    }
+
+    for(auto & countrie : countries) {
+        std::cout << countrie.first << " " << countrie.second << std::endl;
+    }
+
     return 0;
 }
